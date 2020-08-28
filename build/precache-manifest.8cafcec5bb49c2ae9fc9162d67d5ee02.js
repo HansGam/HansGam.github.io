96acbfda87865e9caf3aac20309e529c43d7d16a
HansGam <hgamboa@ucsd.edu>
2020-08-28 12:01:34 -0700

    Redirect

---
+++ build/precache-manifest.8cafcec5bb49c2ae9fc9162d67d5ee02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56c8d08393998c46ff46c74c2c9b414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7ee9d31c07a0eb064b0",
+    "url": "/static/css/main.dcd6174b.chunk.css"
+  },
+  {
+    "revision": "0f5e5edb020a169a8864",
+    "url": "/static/js/2.ad350156.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ad350156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ee9d31c07a0eb064b0",
+    "url": "/static/js/main.5cd74a5c.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  }
+]);
