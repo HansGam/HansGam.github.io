31a8f899542ee47c906e74f922fd9df4d467f9cd
HansGam <hgamboa@ucsd.edu>
2020-08-30 23:11:46 -0700

    Routing updates

---
+++ build/precache-manifest.11adc4db1c9b3208f317c932043faccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8031599454fe97e645e6dc4caa7a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b25377b37d10a62d25b",
+    "url": "/static/css/main.5fa28b55.chunk.css"
+  },
+  {
+    "revision": "7fa2efdd6ef4c4db7309",
+    "url": "/static/js/2.1c4d0d01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1c4d0d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b25377b37d10a62d25b",
+    "url": "/static/js/main.c7134ae1.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  }
+]);
